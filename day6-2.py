920528bc41c8813e4fbdfd5dc4b0718efd6cbdb3
Mike Wilson <mike@soarcast.com>
2023-12-08 22:35:32 -0800

    day 7 checkpoint.

--- day6-2.py
+++ day6-2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 from pprint import pprint
-from collections import OrderedDict
 
 # filename = "day6-test.txt"
 filename = "day6-input.txt"
---
+++ day7.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from pprint import pprint
+
+filename = "day7-test.txt"
+# filename = "day7-input.txt"
+
+rawfile = open(filename, 'r').read()
+rawdata = rawfile.split("\n")
+
