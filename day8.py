53a7206053ab30b61ae09d8a9cd3c7458a34b74e
Mike Wilson <mike@soarcast.com>
2023-12-10 09:44:41 -0800

    day8-1 in the bag!

--- day8.py
+++ day8.py
@@ -1,11 +1,40 @@
 #!/usr/bin/env python3
 from pprint import pprint
 
-filename = "day8-test.txt"
-# filename = "day8-input.txt"
+# filename = "day8-test.txt"
+filename = "day8-input.txt"
+step = 0
+stepcount = 0
+dirs = {
+    'R':1,
+    'L':0
+}
+def nextstep():
+    global step
+    step += 1
+    if step >= len(steps):
+        step = 0
+    return step
 
+start = 'AAA'
+end = 'ZZZ'
 rawfile = open(filename, 'r').read()
-steps = set(rawfile.split("\n\n")[0])
+steps = list(rawfile.split("\n\n")[0])
 rawnetwork = [n.split(" = ") for n in rawfile.split("\n\n")[1].split("\n")]
+network = {}
+pprint("steps: %s" % steps)
+for node, elements in rawnetwork:
+    network[node] = elements.split("(")[1].split(")")[0].split(", ")
+    pprint("node: %s, elements: %s" % (node,network[node]))
 
-pprint(rawnetwork)
+loc = start
+while loc != end:
+    node = network[loc]
+    dir = steps[step]
+    pprint("node: %s, step %s: dir:%s" % (loc,step,dir))
+    loc = node[dirs[dir]]
+    stepcount += 1
+    nextstep()
+
+pprint("stepcount: %s" % stepcount)
+
