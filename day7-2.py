f047dad2b52d2e343607caa1277f2ae33f5341dc
Mike Wilson <mike@soarcast.com>
2023-12-09 16:55:35 -0800

    day 7-2 working!  super clean solution found on the internet made this change easy!  thank you stranger for teaching me new approaches to stuff!

---
+++ day7-2.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+from pprint import pprint
+
+# filename = "day7-test.txt"
+filename = "day7-input.txt"
+
+rawfile = open(filename, 'r').read()
+rawhands = [x.split(" ") for x in rawfile.split("\n")]
+
+def handtype(hand: str) -> int:
+    ordered_types = [
+        [5],        # Five of a kind
+        [4,1],      # Four of a kind
+        [3,2],      # Full house
+        [3,1,1],    # Three of a kind
+        [2,2,1],    # Two pair
+        [2,1,1,1],  # One pair
+        [1,1,1,1,1] # High card
+    ]
+    label_counts = [hand.count(card) for card in set(hand) if card != 'J'] \
+        or [0]
+    label_counts.sort(reverse=True)
+    label_counts[0] += hand.count('J')
+    return ordered_types.index(label_counts)
+
+def cards_as_int(hand: str) -> tuple:
+    ordered_labels = 'AKQT98765432J'
+    return (ordered_labels.index(card) for card in hand)
+
+hands = []
+for hand, bid in rawhands:
+    encoded_hand = (
+        handtype(hand),
+        *cards_as_int(hand),
+        int(bid)
+    )
+    hands.append(encoded_hand)
+
+hands.sort(reverse=True)
+winnings = sum(rank * hand[-1] for rank, hand in enumerate(hands, start=1))
+
+print(winnings)
